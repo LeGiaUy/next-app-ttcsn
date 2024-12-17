6c4a4337d53bc59eb70cb7e8073985270f5c8826
Le Gia Uy <leuy091104@gmail.com>
2024-12-18 06:51:43 +0700

    fpx page

--- app/page.tsx
+++ app/page.tsx
@@ -6,44 +6,40 @@ import HomeBanner from "./components/HomeBanner";
 import NullData from "./components/NullData";
 import ProductCard from "./components/products/productCard";
 
-interface HomeProps{
-  searchParams: Promise<IProductParams>
+interface HomeProps {
+  searchParams: IProductParams; // Nhận searchParams trực tiếp
 }
 
-export default async function Home(props: HomeProps) {
-  const searchParams = await props.searchParams;
-  const products = await getProducts(searchParams)
+export default async function Home({ searchParams }: HomeProps) {
+  const products = await getProducts(searchParams);
 
-  if(products.length === 0){
-    return <NullData title="Không có sản phẩm, ấn tất cả để xóa bộ lọc"/>
+  if (products.length === 0) {
+    return <NullData title="Không có sản phẩm, ấn tất cả để xóa bộ lọc" />;
   }
 
-  function shuffleArray(array: any){
-    for(let i =array.length-1; i>0; i--){
-      const j = Math.floor(Math.random()*(i+1));
-      [array[i], array[j]] = [array[j], array[i]]
+  function shuffleArray(array: any) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
 
     return array;
   }
 
-  const shuffeledProducts = shuffleArray(products)
+  const shuffledProducts = shuffleArray(products);
 
   return (
     <div>
       <Container>
         <div>
-          <HomeBanner></HomeBanner>
+          <HomeBanner />
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {products.map((product: any) => {
-            return(
-              <ProductCard key={product.id} data={product}></ProductCard>
-            )
-          }
-          )}
+          {shuffledProducts.map((product: any) => (
+            <ProductCard key={product.id} data={product} />
+          ))}
         </div>
       </Container>
     </div>
-  )
+  );
 }
