0982ef63cb14c114fc82f8d67046277c5f07e454
Le Gia Uy <leuy091104@gmail.com>
2024-12-18 07:40:41 +0700

    fix bugs

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,13 @@ import HomeBanner from "./components/HomeBanner";
 import NullData from "./components/NullData";
 import ProductCard from "./components/products/productCard";
 
-// Định nghĩa các props mà Home component nhận được
+// Define props that the Home component expects
 interface HomeProps {
-  searchParams: IProductParams; // Ensure searchParams is of type IProductParams, not a Promise
-  products: any[]; // You can change this type according to your needs
+  searchParams: IProductParams; // Use the correct type for searchParams
 }
 
-// Sử dụng async function để lấy dữ liệu trực tiếp trong page component
 export default async function Home({ searchParams }: HomeProps) {
-  // Lấy sản phẩm từ API
+  // Fetch products based on searchParams
   const products = await getProducts(searchParams);
 
   if (products.length === 0) {
@@ -22,9 +20,7 @@ export default async function Home({ searchParams }: HomeProps) {
   return (
     <div>
       <Container>
-        <div>
-          <HomeBanner />
-        </div>
+        <HomeBanner />
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {products.map((product) => (
             <ProductCard key={product.id} data={product} />
