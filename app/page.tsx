ca0c2e310bafd09d4cc436f2e5dd7434052f99e2
Le Gia Uy <leuy091104@gmail.com>
2024-12-18 13:49:12 +0700

    Change Next Version

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,49 @@
-"use client"; // Add this at the top of the file to mark it as a client component
+export const revalidate = 0;
 
 import getProducts, { IProductParams } from "@/actions/getProducts";
-import { useSearchParams } from "next/navigation"; // Import useSearchParams
 import Container from "./components/container";
 import HomeBanner from "./components/HomeBanner";
 import NullData from "./components/NullData";
 import ProductCard from "./components/products/productCard";
 
-// Define the props interface
-interface HomeProps {
-  products: any[];
+interface HomeProps{
+  searchParams: Promise<IProductParams>
 }
 
-export default async function Home() {
-  const searchParams = useSearchParams(); // Get the search params from the URL
+export default async function Home(props: HomeProps) {
+  const searchParams = await props.searchParams;
+  const products = await getProducts(searchParams)
 
-  // Handle the case where searchParams might be null
-  const params: IProductParams = {
-    category: searchParams?.get("category") || null,
-    searchTerm: searchParams?.get("searchTerm") || null,
-  };
-
-  // Fetch products based on the parameters
-  const products = await getProducts(params) || []; // Ensure it's always an array
-
-  if (products.length === 0) {
-    return <NullData title="Không có sản phẩm" />;
+  if(products.length === 0){
+    return <NullData title="Không có sản phẩm, ấn tất cả để xóa bộ lọc"/>
   }
+
+  function shuffleArray(array: any){
+    for(let i =array.length-1; i>0; i--){
+      const j = Math.floor(Math.random()*(i+1));
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+
+    return array;
+  }
+
+  const shuffeledProducts = shuffleArray(products)
 
   return (
     <div>
       <Container>
-        <HomeBanner />
+        <div>
+          <HomeBanner></HomeBanner>
+        </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {products.map((product) => (
-            <ProductCard key={product.id} data={product} />
-          ))}
+          {products.map((product: any) => {
+            return(
+              <ProductCard key={product.id} data={product}></ProductCard>
+            )
+          }
+          )}
         </div>
       </Container>
     </div>
-  );
+  )
 }
