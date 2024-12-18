fd2c7366f151ccad7525f6089454493eed4b47d0
Le Gia Uy <leuy091104@gmail.com>
2024-12-18 07:34:38 +0700

    search params bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,43 +6,33 @@ import HomeBanner from "./components/HomeBanner";
 import NullData from "./components/NullData";
 import ProductCard from "./components/products/productCard";
 
-interface HomeProps{
-  searchParams: IProductParams
+// Định nghĩa các props mà Home component nhận được
+interface HomeProps {
+  searchParams: IProductParams;
+  products: any[];  // Bạn có thể thay đổi kiểu dữ liệu này theo yêu cầu
 }
 
-export default async function Home({searchParams}: HomeProps) {
-  const products = await getProducts(searchParams)
+// Sử dụng async function để lấy dữ liệu trực tiếp trong page component
+export default async function Home({ searchParams }: { searchParams: IProductParams }) {
+  // Lấy sản phẩm từ API
+  const products = await getProducts(searchParams);
 
-  if(products.length === 0){
-    return <NullData title="Không có sản phẩm, ấn tất cả để xóa bộ lọc"/>
+  if (products.length === 0) {
+    return <NullData title="Không có sản phẩm" />;
   }
-
-  function shuffleArray(array: any){
-    for(let i =array.length-1; i>0; i--){
-      const j = Math.floor(Math.random()*(i+1));
-      [array[i], array[j]] = [array[j], array[i]]
-    }
-
-    return array;
-  }
-
-  const shuffeledProducts = shuffleArray(products)
 
   return (
     <div>
       <Container>
         <div>
-          <HomeBanner></HomeBanner>
+          <HomeBanner />
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {products.map((product: any) => {
-            return(
-              <ProductCard key={product.id} data={product}></ProductCard>
-            )
-          }
-          )}
+          {products.map((product) => (
+            <ProductCard key={product.id} data={product} />
+          ))}
         </div>
       </Container>
     </div>
-  )
+  );
 }
